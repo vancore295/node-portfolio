7c86d5c752dcbc64b89ecaff686b7d3feb0a4a3d
vancore295 <silverfire295@gmail.com>
2017-08-13 18:18:34 -0400

    added bootstrap 4 and its dependices injected them into client json file to be auted injected into the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  {}
+];
 
 @NgModule({
   declarations: [
