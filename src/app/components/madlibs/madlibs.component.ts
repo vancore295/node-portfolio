5b3f8bf6d1f42dab87169238cf75a6f0530e5b8f
vancore295 <silverfire295@gmail.com>
2017-08-23 09:26:13 -0400

    begin adding forms for madlibs

--- src/app/components/madlibs/madlibs.component.ts
+++ src/app/components/madlibs/madlibs.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup, FormBuilder, ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-madlibs',
   templateUrl: './madlibs.component.html',
   styleUrls: ['./madlibs.component.css']
 })
+
+
 export class MadlibsComponent implements OnInit {
 
-  constructor() { }
+  greet: FormGroup;
+  let  = new FormGroup({
+    noun_plural: new FormControl()
+  });
 
-  ngOnInit() {
+  constructor() { 
+    
   }
+
+  ngOnInit() { }
 
 }
