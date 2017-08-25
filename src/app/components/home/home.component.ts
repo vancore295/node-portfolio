1d2667d4b3e2dc36ad0b8c52201ead74cc44d502
vancore295 <silverfire295@gmail.com>
2017-08-25 11:28:04 -0400

    fixed errors for displaying data on menuitems added place holders data for menuitems to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,7 +11,68 @@ import { MenuItem } from '../../classes/menuitem';
 export class HomeComponent implements OnInit {
   menuItems: MenuItem[];
 
-  constructor() { }
+  constructor() {
+    this.menuItems = [{
+      route: 'unityGames',
+      href: '',
+      target: '',
+      linkLabel: 'Unity Games',
+      imageSource: '',
+      dataToggle: '',
+      dataPlacement: '',
+      title: '',
+      showImage: false,
+      showLabel: true
+  },
+  {
+      route: 'shop',
+      href: '',
+      target: '',
+      linkLabel: 'Shop',
+      imageSource: '',
+      dataToggle: '',
+      dataPlacement: '',
+      title: '',
+      showImage: false,
+      showLabel: true
+  },
+  {
+      route: 'resume',
+      href: '',
+      target: '',
+      linkLabel: 'Resume',
+      imageSource: '',
+      dataToggle: '',
+      dataPlacement: '',
+      title: '',
+      showImage: false,
+      showLabel: true
+  },
+  {
+      route: 'gameJams',
+      href: '',
+      target: '',
+      linkLabel: 'Game Jams',
+      imageSource: '',
+      dataToggle: '',
+      dataPlacement: '',
+      title: '',
+      showImage: false,
+      showLabel: true
+  },
+  {
+      route: '',
+      href: 'https://github.com/vancore295',
+      target: '_blank',
+      linkLabel: 'Github',
+      imageSource: 'http://res.cloudinary.com/dm210vfbb/image/upload/v1502672153/GitHub-Mark-120px-plus_c7f6jb.png',
+      dataToggle: 'tooltip',
+      dataPlacement: 'left',
+      title: 'Github',
+      showImage: true,
+      showLabel: false
+  }];
+  }
 
   ngOnInit() {
   }
