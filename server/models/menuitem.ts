1d73f185ed164d594081d7323fe6045f2081962b
vancore295 <silverfire295@gmail.com>
2017-08-25 14:44:23 -0400

    removed old js server related files added in controller and routes for menuitems for testing

---
+++ server/models/menuitem.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+
+const menuitemSchema = new mongoose.Scheema({
+    route: String,
+    href: String,
+    target: String,
+    linkLabel: String,
+    imageSource: String,
+    dataToggle: String,
+    dataPlacement: String,
+    title: String,
+    showImage: Boolean,
+    showLabel: Boolean
+});
+
+const MenuItem = mongoose.model('MenuItem', menuitemSchema);
+
+export default MenuItem;
