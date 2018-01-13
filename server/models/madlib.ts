ee5d1338c16a5eb136ea5982d1b5942997227f3d
vancore295 <silverfire295@gmail.com>
2018-01-13 00:00:21 -0500

    added first draft models for madlibs

--- server/models/madlib.ts
+++ server/models/madlib.ts
@@ -1,7 +1,9 @@
 import * as mongoose from 'mongoose';
 
 const madlibSchema = new mongoose.Schema({
-
+    creator: String,
+    madlib: String,
+    data: Object
 });
 
 const Madlib = mongoose.model('Madlib', madlibSchema);
---
+++ src/app/classes/madlib.ts
@@ -0,0 +1,5 @@
+export class Madlib {
+    creator: string;
+    madlib: string;
+    data: Object;
+}
